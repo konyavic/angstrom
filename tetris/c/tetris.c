f34c64e2d2ca84a1216763efcf70cff704364a51
konyavic <konya@nozomi.local>
2015-11-03 16:14:59 +0900

    wip, utf8

--- tetris/c/tetris.c
+++ tetris/c/tetris.c
@@ -1,10 +1,15 @@
-
+#include <locale.h>
 #include <ncurses.h>
 
 int main()
-{   
+{
+    setlocale( LC_ALL, "" );   
     initscr();          /* Start curses mode          */
     printw("Hello World !!!");  /* Print Hello World          */
+    mvaddstr(5, 10, "〠");
+    mvaddstr(5, 12, "〠");
+    mvaddstr(4, 11, "〠");
+    mvaddstr(6, 11, "〠");
     refresh();          /* Print it on to the real screen */
     getch();            /* Wait for user input */
     endwin();           /* End curses mode        */
